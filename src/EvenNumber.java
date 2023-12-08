c31a99f9c5fdeeec9c3b62f6d0b7bdd6fcaccb0a
ahmetberkyesilbas <ahmetberkyesilbas@gmail.com>
2023-12-08 15:40:05 -0500

    Initial commit

---
+++ src/EvenNumber.java
@@ -0,0 +1,11 @@
+public class EvenNumber {
+    public static boolean numberEven(int num){
+        boolean isEven;
+
+        if (num % 2 == 0)
+            isEven = true;
+        else
+            isEven = false;
+        return isEven;
+    }
+}
